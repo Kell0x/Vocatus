90b6a3c589df3506d931f43a9199b8992181723c
Kell0x <michardiere@et.esiea.fr>
2018-06-17 02:47:01 +0100

    update
    au boulot

--- Vocatus/app/src/main/java/com/erwan/vocatus/ListActivity.java
+++ Vocatus/app/src/main/java/com/erwan/vocatus/ListActivity.java
@@ -1,36 +1,48 @@
 package com.erwan.vocatus;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 
 public class ListActivity extends AppCompatActivity {
 
-    boolean mVisible = false;
-
-    //ImageView ImageViewAloha = findViewById(R.id.imageViewAloha);
-    //ImageButton button1 = findViewById(R.id.imageButton33);
+    FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        /*ImageViewAloha.setVisibility(View.GONE);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(mVisible){
-                    ImageViewAloha.setVisibility(View.GONE);
-                    mVisible = false;
-                } else {
-                    ImageViewAloha.setVisibility(View.VISIBLE);
-                    mVisible = true;
-                }
-            }
-        });*/
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + " = ?";
+        String[] selectionArgs = { "My Title" };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
+
+        Cursor cursor = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+
     }
 }
