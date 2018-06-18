9d2806ac79f47e4247c08a967a804aca79cef306
Kell0x <michardiere@et.esiea.fr>
2018-06-18 20:03:02 +0100

    fini

--- Vocatus/app/src/main/java/com/erwan/vocatus/FullscreenActivity.java
+++ Vocatus/app/src/main/java/com/erwan/vocatus/FullscreenActivity.java
@@ -2,6 +2,7 @@ package com.erwan.vocatus;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,178 +11,230 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.MobileAds;
 
 public class FullscreenActivity extends AppCompatActivity {
 
-    private static final boolean AUTO_HIDE = true;
+        private static final boolean AUTO_HIDE = true;
 
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+        private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
+        private static final int UI_ANIMATION_DELAY = 300;
+        private final Handler mHideHandler = new Handler();
+        private View mContentView;
+        private final Runnable mHidePart2Runnable = new Runnable() {
+            @SuppressLint("InlinedApi")
+            @Override
+            public void run() {
+                // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                // Note that some of these constants are new as of API 16 (Jelly Bean)
+                // and API 19 (KitKat). It is safe to use them, as they are inlined
+                // at compile-time and do nothing on earlier devices.
+                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
             }
+        };
+        private View mControlsView;
+        private final Runnable mShowPart2Runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Delayed display of UI elements
+                mControlsView.setVisibility(View.VISIBLE);
+            }
+        };
+        private boolean mVisible;
+        private final Runnable mHideRunnable = new Runnable() {
+            @Override
+            public void run() {
+                hide();
+            }
+        };
+        /**
+         * Touch listener to use for in-layout UI controls to delay hiding the
+         * system UI. This is to prevent the jarring behavior of controls going away
+         * while interacting with activity UI.
+         */
+        private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (AUTO_HIDE) {
+                    delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                }
+                return false;
+            }
+        };
+
+        private static final int CAMERA_PIC_REQUEST = 1111;
+        private ImageView mImage;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.activity_fullscreen);
+
+            MobileAds.initialize(this, "ca-app-pub-5273829862765432~8551228196");
+
+            Button buttonTag = findViewById(R.id.buttonTags);
+            Button buttonList = findViewById(R.id.buttonList);
+            Button buttonCock = findViewById(R.id.buttonCocktails);
+
+            Toast.makeText(getBaseContext(), "Vocatus atque non vocatus", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getBaseContext(), "Deus aderit", Toast.LENGTH_SHORT).show();
+
+            mVisible = false;
+            mControlsView = findViewById(R.id.fullscreen_content_controls);
+            mContentView = findViewById(R.id.fullscreen_content);
+
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+
+
+            // Set up the user interaction to manually show or hide the system UI.
+            mContentView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    toggle();
+                }
+            });
+
+            buttonTag.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    tagsView(view);
+                }
+            });
+            buttonList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listView(view);
+                }
+            });
+            buttonCock.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    cockView(view);
+                }
+            });
+
+            Button b = (Button) findViewById(R.id.button1);
+            b.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if (isExternalStorageWritable()) {
+                        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, CAMERA_PIC_REQUEST);
+                    }
+                }
+            });
+
+            Button c = (Button) findViewById(R.id.button2);
+            c.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if (isExternalStorageReadable()) {
+                        Intent intent = new Intent();
+                        intent.setAction(android.content.Intent.ACTION_VIEW);
+                        intent.setType("image/*");
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        /* Checks if external storage is available for read and write */
+        public boolean isExternalStorageWritable() {
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state)) {
+                return true;
+            }
+            Toast.makeText(this, "External Storage not writable", Toast.LENGTH_SHORT).show();
             return false;
         }
-    };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_fullscreen);
-
-        MobileAds.initialize(this, "ca-app-pub-5273829862765432~8551228196");
-
-        Button buttonTag = findViewById(R.id.buttonTags);
-        Button buttonList = findViewById(R.id.buttonList);
-        Button buttonCock = findViewById(R.id.buttonCocktails);
-
-        Toast.makeText(getBaseContext(), "Vocatus atque non vocatus", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getBaseContext(), "Deus aderit", Toast.LENGTH_SHORT).show();
-
-        mVisible = false;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
+        /* Checks if external storage is available to at least read */
+        public boolean isExternalStorageReadable() {
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state) ||
+                    Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+                return true;
+            }
+            Toast.makeText(this, "External Storage not readable", Toast.LENGTH_SHORT).show();
+            return false;
         }
 
+        @Override
+        protected void onPostCreate(Bundle savedInstanceState) {
+            super.onPostCreate(savedInstanceState);
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        buttonTag.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tagsView(view);
-            }
-        });
-        buttonList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                listView(view);
-            }
-        });
-        buttonCock.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                cockView(view);
-            }
-        });
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
+            // Trigger the initial hide() shortly after the activity has been
+            // created, to briefly hint to the user that UI controls
+            // are available.
+            delayedHide(100);
         }
-    }
 
-    private void hide() {
-        // Hide UI first
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
+        private void toggle() {
+            if (mVisible) {
+                hide();
+            } else {
+                show();
+            }
+        }
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
+        private void hide() {
+            // Hide UI first
+            mControlsView.setVisibility(View.GONE);
+            mVisible = false;
 
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        //mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        //        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
+            // Schedule a runnable to remove the status and navigation bar after a delay
+            mHideHandler.removeCallbacks(mShowPart2Runnable);
+            mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+        }
 
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
+        @SuppressLint("InlinedApi")
+        private void show() {
+            // Show the system bar
+            //mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            //        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+            mVisible = true;
 
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
+            // Schedule a runnable to display UI elements after a delay
+            mHideHandler.removeCallbacks(mHidePart2Runnable);
+            mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+        }
 
-    public void tagsView(View view) {
-        Intent intent = new Intent(this, TagsActivity.class);
-        startActivity(intent);
+        private void delayedHide(int delayMillis) {
+            mHideHandler.removeCallbacks(mHideRunnable);
+            mHideHandler.postDelayed(mHideRunnable, delayMillis);
+        }
 
-    }
+        public void tagsView(View view) {
+            Intent intent = new Intent(this, TagsActivity.class);
+            startActivity(intent);
 
-    public void listView(View view) {
-        Intent intent = new Intent(this, ListActivity.class);
-        startActivity(intent);
-    }
+        }
 
-    public void cockView(View view) {
-        Intent intent = new Intent(this, CockActivity.class);
-        startActivity(intent);
-    }
+        public void listView(View view) {
+            Intent intent = new Intent(this, ListActivity.class);
+            startActivity(intent);
+        }
+
+        public void cockView(View view) {
+            Intent intent = new Intent(this, CockActivity.class);
+            startActivity(intent);
+        }
 }
